3427595b4bcfd0848f869c16362c4412a954dd30
gmlukario <gmlukario@gmail.com>
2022-10-06 23:44:11 -0500

    🚧: define constants

---
+++ src/constants.rs
@@ -0,0 +1,7 @@
+pub const SHIP_HEIGHT: f32 = 25.0;
+pub const SHIP_BASE: f32 = 22.0;
+
+pub const HALF_SHIP_HEIGHT: f32 = SHIP_HEIGHT / 2.0;
+pub const HALF_SHIP_BASE: f32 = SHIP_BASE / 2.0;
+
+pub const BULLET_RADIUS: f32 = 4.0;
